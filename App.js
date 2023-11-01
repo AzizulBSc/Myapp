7cd871e745c0c9d68d73998bdcb8c4d3d5effeed
Azizul Hoque <azizulh8774@gmail.com>
2023-11-02 00:17:19 +0600

    react navigation added

--- App.js
+++ App.js
@@ -1,79 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import axios from 'axios';
-// import { AppRegistry } from 'react-native';
-
 import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
 } from 'react-native-paper';
-import HomeScreen from './src/HomeScreen';
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'lightblue',
-    secondary: 'yellow',
-  },
-};
+// import HomeScreen from './src/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Categories from './src/Category/Categories';
+import Category from './src/Category/Category';
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: 'lightblue',
+//     secondary: 'yellow',
+//   },
+// };
+const Stack = createStackNavigator();
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    // Replace this URL with the API you want to fetch data from
-    axios
-      .get('https://bwmriapp.com/api/category')
-      .then(response => {
-        setData(response.data.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data: ', error);
-      });
-  }, []);
-
   return (
-    <PaperProvider theme={theme}>
-      <HomeScreen />
-      {/* <View style={styles.container}>
-        <Text style={styles.heading}>BWMRIAPP</Text>
-        <FlatList
-          data={data}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-              <Text style={styles.title}>Title: {item.name}</Text>
-              <Text style={styles.body}>ID: {item.id}</Text>
-            </View>
-          )}
-        />
-      </View> */}
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Categories">
+          <Stack.Screen name="Categories" component={Categories} />
+          <Stack.Screen name="Category" component={Category} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#f5f5f5',
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
-  itemContainer: {
-    backgroundColor: '#fff',
-    padding: 16,
-    marginBottom: 16,
-    borderRadius: 8,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  body: {
-    fontSize: 14,
-  },
-});
 
 export default App;
--- src/Category/Categories.jsx
+++ src/Category/Categories.jsx
@@ -1,16 +1,11 @@
 import { useState, useEffect, React } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
 import { Card, Title, Icon, MD3Colors } from 'react-native-paper';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/core';
 const Categories = () => {
-  // const categories = [
-  //   { title: 'Category 1', description: 'Description of Category 1' },
-  //   { title: 'Category 2', description: 'Description of Category 2' },
-  //   { title: 'Category 3', description: 'Description of Category 3' },
-  // ];
   const [data, setData] = useState([]);
   useEffect(() => {
-    // Replace this URL with the API you want to fetch data from
     axios
       .get('https://bwmriapp.com/api/category')
       .then(response => {
@@ -20,11 +15,37 @@ const Categories = () => {
         console.error('Error fetching data: ', error);
       });
   }, []);
-  console.log(data);
+  const navigation = useNavigation();
+  const handleCategoryCardClick = (category) => { 
+    navigation.navigate('Category', { categoryId: category.id });
+  };
+
+  
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  card: {
+    width: '48%', // Adjust the width as needed to fit your design
+    marginBottom: 16,
+  }
+});
+
+
+
 
   return (
+    <View style={{backgroundColor: theme.colors.primary }}>
     <View style={styles.container}>
       {data.map((category, index) => (
+        <TouchableOpacity key={index}
+          style={styles.cardTouchable}
+          onPress={() => {
+            handleCategoryCardClick(category);
+          }} >
         <Card key={index} style={styles.card}>
           <Card.Content>
             <Icon
@@ -34,28 +55,13 @@ const Categories = () => {
             />
             <Title>{category.name}</Title>
           </Card.Content>
-        </Card>
+          </Card>
+          </TouchableOpacity>
       ))}
+    </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    padding: 16,
-    content: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  },
-  card: {
-    width: '48%', // Adjust the width as needed to fit your design
-    marginBottom: 16,
-  },
-});
 
 export default Categories;
